853d72f514e16a1c1ccdd9337d1052eb90d26e69
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-03-11 18:08:41 +0000

    week 2 Day 3

---
+++ Week2/Day3/Exercice_xp_ninja.js
@@ -0,0 +1,17 @@
+class Bird {
+    constructor() {
+      console.log("I'm a bird. 🦢");
+    }
+  }
+  
+  class Flamingo extends Bird {
+    constructor() {
+      console.log("I'm pink. 🌸");
+      super();
+    }
+  }
+  
+  const pet = new Flamingo();
+// output is   :  
+// I'm pink. 🌸
+//I'm a bird. 🦢
