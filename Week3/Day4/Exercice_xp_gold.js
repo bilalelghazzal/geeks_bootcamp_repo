52d50af57370e523603221de8c4685c8d77993c4
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-03-13 13:25:05 +0000

    week 3 Day 4

---
+++ Week3/Day4/Exercice_xp_gold.js
@@ -0,0 +1,26 @@
+const promise1 = Promise.resolve(3);
+const promise2 = 42;
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(resolve, 3000, 'foo');
+});
+// expected output: Array [3, 42, "foo"]
+
+Promise.all([promise1, promise2, promise3]).then(values => {
+  console.log(values);
+});
+// promise all well take all of these three promises and dispmay them 
+
+function timesTwoAsync(x) {
+    return new Promise(resolve => resolve(x * 2)); // return the var x multiple par 2 
+  }
+  
+  const arr = [1, 2, 3];
+  const promiseArr = arr.map(timesTwoAsync); // [2,4,6]
+
+  Promise.all(promiseArr) /* Promise.all combines them, so the result will be [2, 4, 6] once all promises resolve. */ 
+  
+    .then(result => {
+      console.log(result); 
+    });
+
+// analyse code below 
