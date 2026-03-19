6832e2f28f0e646ca382a976788bdf2b2414141f
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-03-19 11:45:34 +0000

    commit add UI

---
+++ Week3/Day5/Dailly_Chall_Convert/script.js
@@ -0,0 +1,13 @@
+const sitch_btn=document.getElementById("switch-currencies");
+const from=document.getElementById("currency-converter");
+const To =document.getElementById("");
+const switchBtn = document.querySelector('.switch-btn');
+
+const form = document.getElementById('currency-converter');
+const fromCurrency = document.getElementById('from-currency');
+const toCurrency = document.getElementById('to-currency');
+const amountInput = document.getElementById('amount');
+const resultDiv = document.getElementById('result');
+
+
+
