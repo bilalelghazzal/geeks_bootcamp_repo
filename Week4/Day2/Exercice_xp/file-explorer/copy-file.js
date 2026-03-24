7444dc1dac2f37afafc3b594ff232cd4f9a17201
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-03-24 17:15:43 +0100

    Week 4 Day 2

--- Week4/Day2/Exercice_xp/file-explorer/copy-file.js
+++ Week4/Day2/Exercice_xp/file-explorer/copy-file.js
@@ -1,16 +1,17 @@
 const fs = require("fs");
 const path = require("path");
 
-const sourcePath = path.join(__dirname, "source.txt");
+const sourcePath = path.join(__dirname, "source.txt");// == (path.dirname,"source.txt")
 const destinationPath = path.join(__dirname, "destination.txt");
 
-fs.readFile(sourcePath, "utf8", (readError, data) => {
+fs.readFile(sourcePath, "utf8", (readError, data) => { // callback function (readError,data) func exec after 
   if (readError) {
-    console.error("Error reading source.txt:", readError.message);
-    return;
+    console.error("Error reading source.txt:", readError.message); // fail
+    return;  // Suecces 
   }
 
-  fs.writeFile(destinationPath, data, "utf8", (writeError) => {
+
+fs.writeFile(destinationPath, data, "utf8", (writeError) => {
     if (writeError) {
       console.error("Error writing destination.txt:", writeError.message);
       return;
