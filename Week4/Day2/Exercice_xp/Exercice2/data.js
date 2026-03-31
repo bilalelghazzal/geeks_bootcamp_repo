f81d92fdcb0d75e550b9ef8d76b14c44e4b43228
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-03-31 17:07:53 +0100

    week 4 Day 3

--- Week4/Day2/Exercice_xp/Exercice2/data.js
+++
@@ -1,7 +0,0 @@
-const people = [
-  { name: "Alice", age: 30, location: "New York" },
-  { name: "Bob", age: 25, location: "Los Angeles" },
-  { name: "Charlie", age: 35, location: "Chicago" }
-];
-
-export default people;
