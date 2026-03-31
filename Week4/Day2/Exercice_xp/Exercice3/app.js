94939b2d4527efbd74c8c4536e6c94a4a1eecc4d
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-03-31 17:07:53 +0100

    week 4 DAy 3

--- Week4/Day2/Exercice_xp/Exercice3/app.js
+++
@@ -1,8 +0,0 @@
-const { readFile, writeFile } = require("./fileManager");
-
-const data = readFile("Hello Word.txt");
-console.log("File content:", data);
-
-writeFile("Bye Word.txt", "Writing to the file");
-console.log("Content written successfully");
-
--- Week4/Day2/Exercice_xp/npm-beginner/app.js
+++
@@ -1,5 +0,0 @@
-import chalk from "chalk "
-console.log(chalk.blue("Hello world!"));
-console.log(chalk.red.bold("Error message!"));
-console.log(chalk.green("Success message!"));
-console.log(chalk.yellow.bgBlack("Warning!"));
