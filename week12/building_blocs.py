eea7ac89223a5150056810edad55f26f5c1ca244
bilalelghazzal <bilalelghazzal3108@gmail.com>
2026-09-02 23:08:25 +0100

    add inteeligent layer _

---
+++ week12/building_blocs.py
@@ -0,0 +1,14 @@
+# # the trully ai component 
+from openai import OpenAI 
+
+# def basic_intelligence(prompt:str)-> str :
+#     client = OpenAI()
+#     response =client.chat.completions.create(
+#         model="gpt-4o" ,input=prompt)
+#     return response.output_text
+
+# if __name__ == "__main__":
+#     prompt=basic_intelligence(prompt="what is big data ")
+#     print("\n output : " , prompt)
+
+
